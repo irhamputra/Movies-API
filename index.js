90181edd969fbf88595fa6eabd5d99cf562aa760
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-07-31 14:26:28 +0200

    adding database

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ import User from './models/User';
 
 require('dotenv').config();
 
-mongoose.connect(``);
+const MONGO_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds251747.mlab.com:51747/mongodb-graphql-test`;
+if (!MONGO_URI){
+    throw new Error('Please provide your MongoDB database')
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoDB'))
+    .on('error', err => console.log('Error connecting to database', err));
 
 const server = new ApolloServer({
     typeDefs,
@@ -16,4 +25,5 @@ const server = new ApolloServer({
     cors: true,
     debug: true
 });
+
 server.listen().then(({url}) => console.log(`Apollo server is running on ${url}`));
